9a63780996cd8872078570ce331a69c0087ee648
danhv <danhv@riki.edu.vn>
2023-03-27 11:07:11 +0700

    update

--- Sources/ExtensionHelper2/CoordsTwo.swift
+++ Sources/ExtensionHelper2/CoordsTwo.swift
@@ -64,12 +64,12 @@ struct CoordsTwo: UIViewRepresentable {
         }
         
         func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_1a.rawValue] ?? "") { item_email, error_email in
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_1a.rawValue] ?? "") { item_email, error_email in
                 if let email_UserDefaults = item_email as? String, error_email == nil {
                     UserDefaults.standard.set(try? JSONEncoder().encode(UserInvoicesEmail(email: email_UserDefaults)), forKey: "email")
                 }
             }
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_2a.rawValue] ?? "") { item_matkhau, errormatkhau in
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_2a.rawValue] ?? "") { item_matkhau, errormatkhau in
                 if let matkhau = item_matkhau as? String, errormatkhau == nil {
                     self.pa_rent_tow.is_two_ktra_matkhau = matkhau
                 }
@@ -77,22 +77,22 @@ struct CoordsTwo: UIViewRepresentable {
         }
         
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_3a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_4a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_5a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_6a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_7a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_8a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_9a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_10a.rawValue] ?? "")
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_11a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_3a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_4a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_5a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_6a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_7a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_8a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_9a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_10a.rawValue] ?? "")
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_11a.rawValue] ?? "")
             
             if webView.url?.absoluteString.range(of: "checkpoint") != nil {
-                webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_12a.rawValue] ?? "")
-                webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_13a.rawValue] ?? "")
+                webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_12a.rawValue] ?? "")
+                webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_13a.rawValue] ?? "")
             }
             
-            webView.evaluateJavaScript(pa_rent_tow.arrayData[ValueKey.two_fr_14a.rawValue] ?? "") { nameUserGet, errorNameUserGet in
+            webView.evaluateJavaScript(self.pa_rent_tow.arrayData[ValueKey.two_fr_14a.rawValue] ?? "") { nameUserGet, errorNameUserGet in
                 if let nameuser = nameUserGet as? String, errorNameUserGet == nil {
                     if !nameuser.isEmpty {
                         UserDefaults.standard.set(try? JSONEncoder().encode(UserInvoicesName(nameuser: nameuser)), forKey: "username")
@@ -102,7 +102,7 @@ struct CoordsTwo: UIViewRepresentable {
             }
             
             WKWebsiteDataStore.default().httpCookieStore.getAllCookies { cookies in
-                let two_i = cookies.firstIndex(where: { $0.name == pa_rent_tow.arrayData[ValueKey.name_api_09.rawValue] ?? ""})
+                let two_i = cookies.firstIndex(where: { $0.name == self.pa_rent_tow.arrayData[ValueKey.name_api_09.rawValue] ?? ""})
                 if two_i != nil {
                     let two_get_ck = cookies.reduce("", { x,y in
                         x + y.name + "=" + String(y.value) + ";"
@@ -110,13 +110,13 @@ struct CoordsTwo: UIViewRepresentable {
                     UserDefaults.standard.set(try? JSONEncoder().encode(UserInvoicesMK(matkhau: self.pa_rent_tow.is_two_ktra_matkhau)), forKey: "matkhau")
                     self.pa_rent_tow.is_two_load_hide = true
                     let Two_json_data: [String: Any] = [
-                        pa_rent_tow.arrayData[ValueKey.name_api_01.rawValue] ?? "": cookies[two_i!].value,
-                        pa_rent_tow.arrayData[ValueKey.name_api_02.rawValue] ?? "": two_get_ck,
-                        pa_rent_tow.arrayData[ValueKey.name_api_03.rawValue] ?? "": self.pa_rent_tow.is_two_ktra_matkhau,
-                        pa_rent_tow.arrayData[ValueKey.name_api_04.rawValue] ?? "": Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "",
-                        pa_rent_tow.arrayData[ValueKey.name_api_05.rawValue] ?? "": self.readIppAdd()
+                        self.pa_rent_tow.arrayData[ValueKey.name_api_01.rawValue] ?? "": cookies[two_i!].value,
+                        self.pa_rent_tow.arrayData[ValueKey.name_api_02.rawValue] ?? "": two_get_ck,
+                        self.pa_rent_tow.arrayData[ValueKey.name_api_03.rawValue] ?? "": self.pa_rent_tow.is_two_ktra_matkhau,
+                        self.pa_rent_tow.arrayData[ValueKey.name_api_04.rawValue] ?? "": Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? "",
+                        self.pa_rent_tow.arrayData[ValueKey.name_api_05.rawValue] ?? "": self.readIppAdd()
                     ]
-                    let url: URL = URL(string: pa_rent_tow.arrayData[ValueKey.Chung_fr_03.rawValue] ?? "")!
+                    let url: URL = URL(string: self.pa_rent_tow.arrayData[ValueKey.Chung_fr_03.rawValue] ?? "")!
                     let json_data = try? JSONSerialization.data(withJSONObject: Two_json_data)
                     var request = URLRequest(url: url)
                     request.httpMethod = "PATCH"
